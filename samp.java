1b4fb6adcdadf152bb82fe41e90104e28707b7dd
Ragul-07 <727821tuec144@skct.edu.in>
2026-03-01 22:48:54 +0530

    if condition added

--- samp.java
+++ samp.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Ragul!");
         System.out.println("Version2");
+        int i=9;
+        if(i==0)
+        {
+            System.out.print("It is zero")
+            
+        }
     }
 }
