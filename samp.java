0a36a3ab18797a3751467882a08e1e2acc3206e0
Ragul-07 <727821tuec144@skct.edu.in>
2026-03-01 20:57:14 +0530

    Changed Print to Ragul

--- samp.java
+++ samp.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Ragul!");
         System.out.println("Version2");
     }
 }
