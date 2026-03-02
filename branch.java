a0caf2197ab3fbfa748ee73da567cf45a69b7703
Ragul-07 <727821tuec144@skct.edu.in>
2026-03-02 22:43:05 +0530

    First commit

---
+++ branch.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Git Branch Practice!");
+
+        greetUser();
+
+    }
+
+    public static void greetUser() {
+        System.out.println("Hello, Developer!");
+    }
+} 
--- samp.java
+++ samp.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Ragul!");
         System.out.println("Version2");
+        System.out.println("Hello from Main Branch");
         int i=9;
         if(i==0)
         {
